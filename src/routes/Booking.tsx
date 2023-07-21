7ce141a0d752a517db1146a114ac251b980e5040
python-game <n.marforio@gmail.com>
2023-07-21 13:44:43 +0200

    add calendar to the booking form

--- src/routes/Booking.tsx
+++ src/routes/Booking.tsx
@@ -3,27 +3,64 @@ import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 
 function Booking() {
-  const [selectedDate, setSelectedDate] = useState<Date | null>(null);
+  const [selectedArrival, setSelectedArrival] = useState<Date | null>(null);
+  const [selectedDeparture, setSelectedDeparture] = useState<Date | null>(null);
+  const [city, setCity] = useState <string> ("")
+  const [direction, setDirection] = useState <string> ("")
+  const [zipCode, setZipCode] = useState <string> ("")
+  const [country,setCountry] = useState <string> ("")
 
+    console.log(city,direction, zipCode,country, selectedArrival,selectedDeparture)
+
+  function handleBookingSubmit(e: React.FormEvent){
+    e.preventDefault()
+    const cityInput = document.getElementById("city") as HTMLInputElement
+    setCity(cityInput?.value)
+    const directionInput = document.getElementById("direction") as HTMLInputElement
+    setDirection(directionInput?.value)
+    const zipCodeInput = document.getElementById("zipcode") as HTMLInputElement
+    setZipCode(zipCodeInput?.value)
+    const countryInput = document.getElementById("country") as HTMLInputElement
+    setCountry(countryInput?.value)
+  }
+  
   return (
     <div className="booking">
       <h1>Booking</h1>
-      <form className="bookingForm">
+      <form className="bookingForm" onSubmit={handleBookingSubmit}>
         <label>Where is your Stay:</label>
-        <input placeholder="City" type="text"></input>
+        <input required placeholder="City" type="text" id="city"></input>
+        <label>Direction:</label>
+        <input required type="text" placeholder="Riegerstrasse 4" id="direction"></input>
         <label>ZIP code:</label>
-        <input type="number" placeholder="6616"></input>
+        <input required type="number" placeholder="6616" id="zipcode"></input>
         <label>Country</label>
-        <input type="text" placeholder="Switzerland"></input>
+        <input required type="text" placeholder="Switzerland" id="country"></input>
+
+        <div className="calendarBooking">
+          <p>from</p>
+          <DatePicker
+            dateFormat={"dd/MM/yyyy"}
+            showMonthDropdown
+            showYearDropdown
+            selected={selectedArrival}
+            onChange={(date) => setSelectedArrival(date)}
+            required
+          />
+          <p>to</p>
+          <DatePicker
+            dateFormat={"dd/MM/yyyy"}
+            showMonthDropdown
+            showYearDropdown
+            selected={selectedDeparture}
+            onChange={(dateDeparture) => setSelectedDeparture(dateDeparture)}
+            required
+          />
+        </div>
+        <button className="bookingButton" type="submit">
+          Save your Stay
+        </button>
       </form>
-      <label>How many Days?</label>
-      <DatePicker
-        dateFormat={"dd/MM/yyyy"}
-        showMonthDropdown
-        showYearDropdown
-        selected={selectedDate}
-        onChange={(date) => setSelectedDate(date)}
-      />
     </div>
   );
 }
