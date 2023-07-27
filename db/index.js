69a97510b5b0b31553813a8802efc4f902a1142b
python-game <n.marforio@gmail.com>
2023-07-27 12:17:27 +0200

    trying to post something in mongodb

--- db/index.js
+++ db/index.js
@@ -1,26 +1,58 @@
+// import Booking from "./model/Booking";
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 
 async function connection() {
   try {
-    await mongoose.connect(""); // put your MongoURL and is working
+    await mongoose.connect(
+      "mongodb+srv://nicolemarforio:yMpXiOBwVt22ofT9@gio-traveapp.jw1r7me.mongodb.net/"
+    ); // put your MongoURL and is working
   } catch (error) {
     console.log(error);
   }
 }
-console.log(process.env);
 
 // For backend and express
 const express = require("express");
 const app = express();
 const cors = require("cors");
+// const { default: Booking } = require("./model/Booking");
 console.log("App listen at port 5000");
 app.use(express.json());
 app.use(cors());
 app.get("/", (req, resp) => {
   resp.send("App is Working");
 });
+
+app.post("/booking", async (req, resp) => {
+  try {
+    const bookingData = new Booking(req.body);
+    let result = await bookingData.save();
+    result = result.toObject();
+    if (result) {
+      resp.send(req.body);
+      console.log(result);
+    } else {
+      console.log("Booking Data saved");
+    }
+  } catch (e) {
+    resp.send("Something Went Wrong");
+  }
+});
+
+const { Schema } = mongoose;
+const bookingSchema = new Schema({
+  arrival: Number,
+  departure: Number,
+  city: String,
+  direction: String,
+  zipCode: String,
+  country: String,
+});
+
+const Booking =
+  mongoose.models.Booking || mongoose.model("Booking ", bookingSchema);
 
 connection();
 app.listen(5000);
--- src/routes/Booking.tsx
+++ src/routes/Booking.tsx
@@ -3,101 +3,126 @@ import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
 
 function Booking() {
-  const [selectedArrival, setSelectedArrival] = useState<Date | null>(null);
+  const [selectedArrival, setSelectedArrival] = useState<Date| null>(null);
   const [selectedDeparture, setSelectedDeparture] = useState<Date | null>(null);
-  const [city, setCity] = useState <string> ("")
-  const [direction, setDirection] = useState <string> ("")
-  const [zipCode, setZipCode] = useState <string> ("")
-  const [country,setCountry] = useState <string> ("")
+  const [city, setCity] = useState<string>("");
+  const [direction, setDirection] = useState<string>("");
+  const [zipCode, setZipCode] = useState<string>("");
+  const [country, setCountry] = useState<string>("");
 
-    console.log(city,direction, zipCode,country, selectedArrival,selectedDeparture)
+  // const arrivalMonth = selectedArrival!.getUTCMonth() + 1; //months from 1-12
+  // const arrivalDay = selectedArrival!.getUTCDate();
+  // const arrivalYear = selectedArrival!.getUTCFullYear();
+  // const newDateArrival = arrivalDay + "/" + arrivalMonth + "/" + arrivalYear;
+  // console.log("New arrival date", newDateArrival);
 
-  async function handleBookingSubmit(e: React.FormEvent){
-    e.preventDefault()
-    const cityInput = document.getElementById("city") as HTMLInputElement
-    setCity(cityInput?.value)
-    const directionInput = document.getElementById("direction") as HTMLInputElement
-    setDirection(directionInput?.value)
-    const zipCodeInput = document.getElementById("zipcode") as HTMLInputElement
-    setZipCode(zipCodeInput?.value)
-    const countryInput = document.getElementById("country") as HTMLInputElement
-    setCountry(countryInput?.value)
+  async function handleBookingSubmit(e: React.FormEvent) {
+    e.preventDefault();
+    const cityInput = document.getElementById("city") as HTMLInputElement;
+    setCity(cityInput?.value);
+    const directionInput = document.getElementById(
+      "direction"
+    ) as HTMLInputElement;
+    setDirection(directionInput?.value);
+    const zipCodeInput = document.getElementById("zipcode") as HTMLInputElement;
+    setZipCode(zipCodeInput?.value);
+    const countryInput = document.getElementById("country") as HTMLInputElement;
+    setCountry(countryInput?.value);
   }
-  
-//   const handleOnSubmit = async (e) => {
-//     e.preventDefault();
-//     let result = await fetch(
-//     'http://localhost:5000', {
-//         method: "post",
-//         body: JSON.stringify({ }),
-//         headers: {
-//             'Content-Type': 'application/json'
-//         }
-//     })
-//     result = await result.json();
-//     console.warn(result);
-//     if (result) {
-//         alert("Data saved succesfully");
-//     }
-// }
+  const bookingObject: object = {
+    city: city,
+    direction: direction,
+    zipCode: zipCode,
+    country: country,
+    selectedArrival: selectedArrival,
+    selectedDeparture: selectedDeparture,
+  };
 
+  async function handleBookingSubmit1 (e: React.FormEvent) {
+      e.preventDefault();
+      let result = await fetch("http://localhost:5000", {
+      method: "post",
+      body: JSON.stringify({bookingObject}),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    result = await result.json();
+    console.warn(result);
+    if (result) {
+      alert("Data saved succesfully");
+    }
+  };
 
-  const linkBooking: Array<string> = ["https://www.booking.com","https://www.hostelworld.com","https://www.airbnb.com"]
-
+  const linkBooking: Array<string> = [
+    "https://www.booking.com",
+    "https://www.hostelworld.com",
+    "https://www.airbnb.com",
+  ];
 
   return (
     <>
-    <div className="menu">
-      <a className="homeLink" href="/">Home</a>
-      <a href="/budget">Budget</a>
-      <a href="/buketlist">BuketList</a>
-    </div>
-    <div className="booking">
-      <h1>Booking</h1>
-      <form className="bookingForm" onSubmit={handleBookingSubmit}>
-        <label>Where is your Stay:</label>
-        <input required placeholder="City" type="text" id="city"></input>
-        <label>Direction:</label>
-        <input required type="text" placeholder="Riegerstrasse 4" id="direction"></input>
-        <label>ZIP code:</label>
-        <input required type="number" placeholder="6616" id="zipcode"></input>
-        <label>Country</label>
-        <input required type="text" placeholder="Switzerland" id="country"></input>
-
-        <div className="calendarBooking">
-          <p>from</p>
-          <DatePicker
-            dateFormat={"dd/MM/yyyy"}
-            showMonthDropdown
-            showYearDropdown
-            selected={selectedArrival}
-            onChange={(date) => setSelectedArrival(date)}
-            required
-          />
-          <p>to</p>
-          <DatePicker
-            dateFormat={"dd/MM/yyyy"}
-            showMonthDropdown
-            showYearDropdown
-            selected={selectedDeparture}
-            onChange={(dateDeparture) => setSelectedDeparture(dateDeparture)}
-            required
-          />
-        </div>
-        <button className="bookingButton" type="submit">
-          Save your Stay
-        </button>
-      </form>
-      <div className="bookingLinks">
-      {linkBooking.map((link)=>{
-        return (
-          <a href={link}>{link}</a>
-        )
-      })}
-      <button>Add new link</button>
-      <button>Delete link</button>
+      <div className="menu">
+        <a className="homeLink" href="/">
+          Home
+        </a>
+        <a href="/budget">Budget</a>
+        <a href="/buketlist">BuketList</a>
       </div>
-    </div>
+      <div className="booking">
+        <h1>Booking</h1>
+        <form className="bookingForm" onSubmit={(e)=>handleBookingSubmit}>
+          <label>Where is your Stay:</label>
+          <input required placeholder="City" type="text" id="city"></input>
+          <label>Direction:</label>
+          <input
+            required
+            type="text"
+            placeholder="Riegerstrasse 4"
+            id="direction"
+          ></input>
+          <label>ZIP code:</label>
+          <input required type="number" placeholder="6616" id="zipcode"></input>
+          <label>Country</label>
+          <input
+            required
+            type="text"
+            placeholder="Switzerland"
+            id="country"
+          ></input>
+
+          <div className="calendarBooking">
+            <p>from</p>
+            <DatePicker
+              dateFormat={"dd/MM/yyyy"}
+              showMonthDropdown
+              showYearDropdown
+              selected={selectedArrival}
+              onChange={(date) => setSelectedArrival(date)}
+              required
+            />
+            <p>to</p>
+            <DatePicker
+              dateFormat={"dd/MM/yyyy"}
+              showMonthDropdown
+              showYearDropdown
+              selected={selectedDeparture}
+              onChange={(dateDeparture) => setSelectedDeparture(dateDeparture)}
+              required
+            />
+          </div>
+          <button className="bookingButton" type="submit">
+            Save your Stay
+          </button>
+        </form>
+        <div className="bookingLinks">
+          {linkBooking.map((link) => {
+            return <a href={link}>{link}</a>;
+          })}
+          <button>Add new link</button>
+          <button>Delete link</button>
+        </div>
+      </div>
     </>
   );
 }
